f0281138fe0c37a38560457a0705a0210b7d55d1
NamataMarkDavid <markdavidnamata03@gmail.com>
2022-09-20 00:02:34 +0700

    first commit

---
+++ exercises/exercise1.php
@@ -0,0 +1,26 @@
+?<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>exercise1</title>
+</head>
+<body>
+<?php
+  echo "<h4 style = 'color : red;'> Twinkle, Twinkle little star!</h4>";
+?>
+
+
+  <?php
+        
+        echo "Twinkle, Twinkle little Star! </br> ";
+        
+        $twinkle = "Bright";
+        $star ="sky";
+        
+        echo " $twinkle, $twinkle little $star! </br> ";
+        
+        ?>
+
+</body>
+</html>

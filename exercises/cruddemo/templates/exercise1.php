8cb0113f68552a3674df7cdee9bf99a464db23f3
NamataMarkDavid <markdavidnamata03@gmail.com>
2022-11-08 14:14:05 +0700

    first commit

---
+++ exercises/cruddemo/templates/exercise1.php
@@ -0,0 +1,14 @@
+?<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>exercise 1</title>
+</head>
+<body>
+	<?php
+      echo “Twinkle, Twinkle little star.”;
+	?>
+
+</body>
+</html>
